30a2e5e19f6694484ddf0994a31ec109e6f3ab5f
Ola-PC <olgakyisla@gmail.com>
2018-09-29 07:52:15 -0700

    Initial commit

---
+++ src/test/java/BasicScript.java
@@ -0,0 +1,78 @@
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicScript {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ola-PC\\IdeaProjects\\selenium-test\\chrome-driver\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        baseUrl = "https://www.google.com.ua/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testBasicScript() throws Exception {
+        driver.get(baseUrl + "/?gws_rd=ssl");
+
+        WebElement input = driver.findElement(By.id("lst-ib"));
+        input.sendKeys("Selenium WebDriver");
+        input.sendKeys(Keys.RETURN);
+
+        driver.findElements(By.className("LC20lb")).get(0).click();
+
+        assertEquals("https://www.seleniumhq.org/projects/webdriver/", driver.getCurrentUrl());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
